41f4bb0293fa21f7709e1709f4913e5362f68b10
kychilly <randomweebkeys@gmail.com>
2025-08-20 23:09:30 -0400

    more questions

--- src/main/java/com/DiscordBot.java
+++ src/main/java/com/DiscordBot.java
@@ -43,7 +43,7 @@ public class DiscordBot {
     public static void main(String[] args) {
         try {
             new DiscordBot();
-            Timer timer = new Timer();
+            // Timer timer = new Timer();
 
         } catch (LoginException e) {
             System.out.println("Error: Invalid bot token - check your .env file");
